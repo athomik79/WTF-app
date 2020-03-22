cf10b6f203be2f02dc0eada5d40dea09da26b68b
Andrew Thomas <athomik79@gmail.com>
2020-03-21 21:32:33 -0400

    added weather and food button functionality, all data displaying correctly, maps are updating

--- ali_lon_lat.js
+++ ali_lon_lat.js
@@ -1,23 +1,64 @@
-var zip = "22030";
+// Los Angeles, CA 
+//var lat = 34.0522;
+//var lon = -118.2437;
+// Ashburn, VA 20148
+//var lat = 39.0403;
+//var lon = -77.4852;
+var lat = localStorage.getItem("lat");
+var lon = localStorage.getItem("lon");
 var APIKey = "5142591fdf2a45c9927600243a8834f2";
-
-var queryURL =  "https://api.weatherbit.io/v2.0/forecast/daily?postal_code="+zip+"&country=US&units=i&key=" + APIKey;
+var queryURL = "https://api.weatherbit.io/v2.0/current?lat=" + lat + "&lon=" + lon + "&units=I&key=" + APIKey;
+var urlString = "https://us-restaurant-menus.p.rapidapi.com/restaurants/search/geo?page=1&lon=" + lon + 
+"&lat=" + lat + "&distance=1";
+var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": urlString,
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "us-restaurant-menus.p.rapidapi.com",
+      "x-rapidapi-key": "b21fc3e940mshe309850232bdc1ap11ff86jsncc48faf41fcd"
+  }
+}
 
 $.ajax({
     url: queryURL,
     method: "GET"
 })
+   .then(function(response) 
+   {
+       console.log(response);
+        var currentDayEl = $("p.currentDay");
+        var currDate = moment().format('MMMM Do YYYY');
+        currentDayEl.text(currDate);
+       var iconcode = response.data[0].weather.icon;
+       var iconurl = "http://weatherbit.io/static/img/icons/" + iconcode + ".png";
+       $("#icon").attr("src",iconurl);
+       $("#desc").text("Description: " + response.data[0].weather.description);
+       var tempF = response.data[0].temp;
+       var degreeSymbol = "\xB0";
+       $("#main").text("Temperature: " + tempF.toFixed(2) + " " + degreeSymbol + "F");
+       $("#city").text(response.data[0].city_name);
+       $("#state").text(response.data[0].state_code);
+    
+       $.ajax(settings)
+        .then(function (response) {
 
-    .then(function(response) {
-        console.log(response);
-
-        var lat = response.lat;
-        console.log(lat);
-
-        var lon = response.lon;
-        console.log(lon);
-
-        localStorage.setItem("lattitude", JSON.stringify(lat));
-
-        localStorage.setItem("longitude", JSON.stringify(lon));
-    })
+        for (var i=0; i<10; i++)
+        {
+          var name = response.result.data[i].restaurant_name;
+          var nameDiv = "<div id=\"name"+i+"\">"+ name + "</div>";
+          $('#food').append(nameDiv);
+          var address = response.result.data[i].address.formatted;
+          var addressDiv = "<div id=\"address"+i+"\">"+ address + "</div>";
+          $('#food').append(addressDiv);
+          var phone = response.result.data[i].restaurant_phone;
+          var phoneDiv = "<div id=\"phone"+i+"\">"+ phone + "</div>";
+          $('#food').append(phoneDiv);
+          var id = response.result.data[i].restaurant_id;
+          var idDiv = "<div id=\"id"+i+"\">"+ id + "</div>";
+          $('#food').append(idDiv);
+          $('#food').append("<br>");
+        }
+    });    
+})
--- script.js
+++ script.js
@@ -69,6 +69,10 @@ $.ajax({
 $("#find").click(function()
 {initMap()});
 
+$('#weather').click(function(){
+    window.location.href='aliindex.html';
+});
+
 
 function initMap () {
 let location = new Object();
@@ -102,4 +106,21 @@ navigator.geolocation.getCurrentPosition(function(pos) {
 
     });   
     });
+    }
+
+    function GetAddress(lat, lng) {
+    // var lat = gLat;
+    // var lng = gLong;
+    var apiKey = "AIzaSyAriXxPMt_ogYg9aHln6QbvoT0FZxW0_og";
+    var url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&key=" + apiKey;
+    alert (lat + "," + lng);
+    alert (url);
+    $.getJSON(url,function (data, textStatus) {
+           var streetaddress=data.results[0].formatted_address;
+            // console.log (streetaddress);
+            console.log (data);
+          localStorage.setItem ("lat",lat);
+          localStorage.setItem ("lon",lng);
+        
+        });
     }
